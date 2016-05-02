5255f0f7e614c7bc43dae89e4f02908cf071fe12
Yoshio HANAWA <foo@example.com>
2016-05-02 16:02:26 +0900

    Support building PHP 7 / Support Mac OS X 10.11 / Support SSLv2-disabled OpenSSL

--- install-all-php.sh
+++ install-all-php.sh
@@ -23,12 +23,12 @@ shift $((OPTIND - 1))
 
 dirname=$(cd $(dirname $0);pwd)
 
-php_gz_pkgs=`find . -maxdepth 1 -type f -name 'php-5.*.*.tar.gz' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.gz$'`
-php_bz2_pkgs=`find . -maxdepth 1 -type f -name 'php-5.*.*.tar.bz2' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.bz2$'`
-php_xz_pkgs=`find . -maxdepth 1 -type f -name 'php-5.*.*.tar.xz' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.xz$'`
+php_gz_pkgs=`find . -maxdepth 1 -type f -name 'php-[57].*.*.tar.gz' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.gz$'`
+php_bz2_pkgs=`find . -maxdepth 1 -type f -name 'php-[57].*.*.tar.bz2' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.bz2$'`
+php_xz_pkgs=`find . -maxdepth 1 -type f -name 'php-[57].*.*.tar.xz' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?\.tar\.xz$'`
 
 if [ -z "$php_gz_pkgs" -a -z "$php_bz2_pkgs" -a -z "$php_xz_pkgs" ]; then
-    echo "cannot access php-5.*.tar.{gz,bz2,xz}: No PHP package found"
+    echo "cannot access php-[57].*.tar.{gz,bz2,xz}: No PHP package found"
     exit
 fi
 
@@ -56,7 +56,7 @@ if [ -n "$php_xz_pkgs" ]; then
     done
 fi
 
-# To compie with gcc4 for PHP 5.0.0-5.0.3
+# Supporting gcc4 for PHP 5.0.0-5.0.3
 #  see: http://bugs.php.net/32150
 
 old_php_dirs=`find . -maxdepth 1 -type d -name 'php-5.0.[0123]*' | egrep '\.[0-3]((alpha|beta|RC)[0-9]+)?$'`
@@ -69,7 +69,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with OpenSSL 1.0+
+# Supporting OpenSSL 1.0+
 #  for PHP 5.0.x, 5.1.x
 #
 # see: http://bugs.php.net/48116
@@ -85,7 +85,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with OpenSSL 1.0+
+# Supporting OpenSSL 1.0+
 #  for PHP 5.2.0-5.2.6
 
 old_php_dirs=`find . -maxdepth 1 -type d -name 'php-5.2.[0-6]'`
@@ -98,7 +98,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with OpenSSL 1.0+
+# Supporting OpenSSL 1.0+
 #  for PHP 5.2.7 - 5.2.12
 #
 # Note: Patching to PHP 5.2.11 and 5.2.12 failed partialy, but no problem.
@@ -113,7 +113,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with OpenSSL 1.0+
+# Supporting OpenSSL 1.0+
 #  for PHP 5.3.0 - 5.3.1
 #
 # Note: Patching to PHP 5.3.1 failed partialy, but no problem.
@@ -128,7 +128,22 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with libxml2 2.9.0+
+# Supporting SSLv2-disabled OpenSSL 
+#  for PHP 5.0.x, 5.1.x, 5.2.x, 5.3.0 - 5.3.6
+#
+# Note: Patching to PHP 5.0.0, 5.0.1 failed partialy, but no problem.
+
+old_php_dirs=`find . -maxdepth 1 -type d \( -name 'php-5.[012].*' -or -name 'php-5.3.[0-6]' \)`
+
+if [ -n "$old_php_dirs" ]; then
+    for php_dir in $old_php_dirs; do
+        cd $php_dir
+        patch -p1 -N < $dirname/patches/patch-to-php-5.3.6-with-openssl-disabled-sslv2.txt
+        cd ..
+    done
+fi
+
+# Supporting libxml2 2.9.0+
 #  for PHP 5.0.x, 5.1.x
 
 old_php_dirs=`find . -maxdepth 1 -type d \( -name 'php-5.0.[0-5]' -or -name 'php-5.1.[0-6]' \)`
@@ -141,7 +156,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with libxml2 2.9.0+
+# Supporting libxml2 2.9.0+
 #  for PHP 5.2.0-5.2.11, 5.3.0
 
 old_php_dirs=`find . -maxdepth 1 -type d \( -name 'php-5.2.[0-9]' -or -name 'php-5.2.1[01]' -or -name 'php-5.3.0' \)`
@@ -154,7 +169,7 @@ if [ -n "$old_php_dirs" ]; then
     done
 fi
 
-# To work with libxml2 2.9.0+
+# Supporting libxml2 2.9.0+
 #  for PHP 5.2.12-5.2.17, 5.3.1-5.3.16, 5.4.0-5.4.6
 
 old_php_dirs=`find . -maxdepth 1 -type d \( -name 'php-5.2.1[2-7]' -or -name 'php-5.3.[1-9]' -or -name 'php-5.3.1[0-6]' -or -name 'php-5.4.[0-6]' \)`
@@ -203,13 +218,15 @@ CONFIGURE_OPTS="--enable-mbstring --disable-cgi --with-zlib --with-bz2 --enable-
 
 case `uname` in
   Darwin)
-    CFLAGS="-fnested-functions"
+    #CFLAGS="-fnested-functions"
     if [ `uname -r | cut -d . -f 1` -ge 10 ]; then
         # add -lresolv for MacOS 10.6 or later
         # via: http://stackoverflow.com/questions/1204440/errors-linking-libresolv-when-building-php-5-2-10-from-source-on-os-x
         EXTRA_LIBS=-lresolv
     fi
-    if [ -f /usr/lib/libssl.dylib ]; then
+    if hash brew 2>/dev/null && [ -n "$(brew list | grep '^openssl$')" ]; then
+        CONFIGURE_OPTS="$CONFIGURE_OPTS --with-openssl=$(brew --prefix openssl)"
+    elif [ -f /usr/lib/libssl.dylib ]; then
         CONFIGURE_OPTS="$CONFIGURE_OPTS --with-openssl=/usr"
     fi
     if [ -f /opt/local/lib/libxml2.dylib ]; then
@@ -218,6 +235,9 @@ case `uname` in
     elif [ -f /usr/local/lib/libxml2.dylib ]; then
         # Homebrew
         CONFIGURE_OPTS="$CONFIGURE_OPTS --with-libxml-dir=/usr/local"
+    elif hash brew 2>/dev/null && [ -n "$(brew list | grep '^libxml2$')" ]; then
+        # Homebrew
+        CONFIGURE_OPTS="$CONFIGURE_OPTS --with-libxml-dir=$(brew --prefix libxml2)"
     else
         # MacOSX stock
         CONFIGURE_OPTS="$CONFIGURE_OPTS --with-libxml-dir=/usr"
@@ -228,6 +248,9 @@ case `uname` in
     elif [ -f /usr/local/lib/libiconv.dylib ]; then
         # Homebrew
         CONFIGURE_OPTS="$CONFIGURE_OPTS --with-iconv=/usr/local"
+    elif hash brew 2>/dev/null && [ -n "$(brew list | grep '^libiconv$')" ]; then
+        # Homebrew
+        CONFIGURE_OPTS="$CONFIGURE_OPTS --with-iconv=$(brew --prefix libiconv)"
     else
         # MacOSX stock libiconv is not compatible, so disabled.
         CONFIGURE_OPTS="$CONFIGURE_OPTS --without-iconv"
@@ -235,7 +258,7 @@ case `uname` in
     ;;
 esac
 
-php_dirs=`find . -maxdepth 1 -type d -name 'php-5.*.*' | egrep '\.[0-9]+((alpha|beta|RC)[0-9]+)?$'`
+php_dirs=`find . -maxdepth 1 -type d -name 'php-[57].*.*' | egrep '\.[0-9]+\.[0-9]+((alpha|beta|RC)[0-9]+)?$'`
 
 MAKE_OPTS=""
 if [ -n "$NJOB" ]; then
